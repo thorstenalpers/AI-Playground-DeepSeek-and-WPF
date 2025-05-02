c37386f7649d5ea7ae4686364067a4dc5e01d3c0
thorstenalpers <thorstenalpers@web.de>
2025-05-02 19:55:51 +0200

    Update to latest chart version

--- infrastructure/deploy-infrastructure.cmd
+++ infrastructure/deploy-infrastructure.cmd
@@ -6,8 +6,8 @@ helm repo add ollama-helm https://otwld.github.io/ollama-helm/
 
 helm repo update
 
-helm upgrade --install open-webui --version 5.1.1 open-webui/open-webui -f ".\charts\open-webui\values.yaml" --wait
-helm upgrade --install ollama ollama-helm/ollama --version 1.4.1 -f ".\charts\ollama\values.yaml" --wait
+helm upgrade --install open-webui --version 6.4.0 open-webui/open-webui -f ".\charts\open-webui\values.yaml" --wait
+helm upgrade --install ollama ollama-helm/ollama --version 1.15.0 -f ".\charts\ollama\values.yaml" --wait
 
-REM helm show values superset/superset --version 0.12.11 > values.yaml
-REM helm show values ollama-helm/ollama --version 1.4.1  > values.yaml
+REM helm show values open-webui/open-webui --version 6.4.0 > values.yaml
+REM helm show values ollama-helm/ollama --version 1.15.0 > values.yaml
