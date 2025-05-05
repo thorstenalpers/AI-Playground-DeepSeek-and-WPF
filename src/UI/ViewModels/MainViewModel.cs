5750167ebc2120a0d9c41e2ee600e42e021af91a
thorstenalpers <thorstenalpers@web.de>
2025-05-05 07:24:04 +0200

    Move logic from code behind to viewmodel

--- src/UI/ViewModels/MainViewModel.cs
+++ src/UI/ViewModels/MainViewModel.cs
@@ -1,10 +1,34 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DeepSeek.WPF.Core.Contracts.Services;
 
 namespace DeepSeek.WPF.UI.ViewModels;
 
-public class MainViewModel : ObservableObject
+public partial class MainViewModel : ObservableObject
 {
-    public MainViewModel()
+    private readonly IAiMessageService _aiMessageService;
+    public MainViewModel(IAiMessageService aiMessageService)
     {
+        _aiMessageService = aiMessageService;
+    }
+
+    [ObservableProperty]
+    private string userInput;
+
+    [ObservableProperty]
+    private string output;
+
+    [RelayCommand]
+    private async Task SendAsync()
+    {
+        Output = string.Empty;
+
+        if (string.IsNullOrWhiteSpace(UserInput))
+        {
+            Output = "Please enter a question.";
+            return;
+        }
+        var result = await _aiMessageService.SendPromptViaLibraryAsync(UserInput);
+        Output = result;
     }
 }
--- src/UI/Views/MainPage.xaml.cs
+++ src/UI/Views/MainPage.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using System.Windows.Controls;
-using DeepSeek.WPF.Core.Contracts.Services;
 using DeepSeek.WPF.UI.ViewModels;
 
 namespace DeepSeek.WPF.UI.Views;
 
 public partial class MainPage : Page
 {
-    private readonly IAiMessageService _aiMessageService;
-    public MainPage(MainViewModel viewModel, IAiMessageService aiMessageService)
+    public MainPage(MainViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
-        _aiMessageService = aiMessageService;
-    }
-
-    private async void SendButton_Click(object sender, System.Windows.RoutedEventArgs e)
-    {
-        OutputTextBox.Text = "";
-        var userInput = UserInputTextBox.Text;
-        var result = await _aiMessageService.SendPromptViaLibraryAsync(userInput);
-        OutputTextBox.Text = result;
     }
 }
